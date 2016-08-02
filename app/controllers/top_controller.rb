0712328f15cac84046fb0e86e0f42405290c84e2
上村治也 <haruyasora423@gmail.com>
2016-08-02 02:12:14 -0700

    完成したっぽい

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,30 @@
 class TopController < ApplicationController
 
-def index
-	@words = Note.all
-end
+
+protect_from_forgery except: :create
+
+	def index
+		 @last = Note.last.id
+
+		@word1 = Note.find(rand(@last) + 1)
+		@word2 = Note.find(rand(@last) + 1)
+		@word3 = Note.find(rand(@last) + 1)
+		@word = Note.new
+	end
+
+		
+
+
+
+	def create
+		@word = Note.create(create_params)
+		redirect_to "/"
+	end
+
+private
+	def create_params
+		params.require(:note).permit(:word)
+
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "top#index"
+  post "/create" => "top#create"
 end
