60da07219b13ca8ac23888c01a8ed7a4ce0870c4
上村治也 <haruyasora423@gmail.com>
2016-08-04 21:42:35 -0700

    ホームをしあげた

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,42 @@
 class TopController < ApplicationController
 
-def index
+
+protect_from_forgery except: :create
+
+	def destroy
+    @word = Note.find(params[:id])
+    @word.destroy
+    redirect_to index_path
+	end
+
+	def slot
+		 @last = Note.last.id
+        @words = Note.all
+		@word1 = Note.find(rand(@last) + 1)
+		@word2 = Note.find(rand(@last) + 1)
+		@word3 = Note.find(rand(@last) + 1)
+		@word = Note.new
+	end
+
+	def index
 	@words = Note.all
-end
+	@word = Note.new
+    end 
+   
+   def show
+   
+   end
+
+
+	def create
+		@word = Note.create(create_params)
+		redirect_to "/index"
+	end
+
+private
+	def create_params
+		params.require(:note).permit(:word)
+
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root "top#index"
+  root "top#show"
+  post "/create" => "top#create"
+  get"/index"=> "top#index" 
+  get"/slot"=> "top#slot"
 end
+
